a684eb1947259b4b78ee8fbeeef949f0660a902c
AlfinhiHD <alfinhihd@gmail.com>
2022-11-08 12:45:41 +0700

    Section 5 done

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -6,8 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements = [{type: 'server', name: 'TestServer', content: 'Just test'}];
   
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverName
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
+
+  onChangeFirst() {
+    this.serverElements[0].name = 'Changed!';
+  }
+
+  onDestroyFirst() {
+    this.serverElements.splice(0, 1);
+  }
 
  
 }
--- my-first-app/src/app/server-element/server-element.component.ts
+++ my-first-app/src/app/server-element/server-element.component.ts
@@ -1,15 +1,80 @@
-import { Component, OnInit } from '@angular/core';
+import { 
+  AfterContentChecked, 
+  AfterContentInit, 
+  AfterViewChecked, 
+  AfterViewInit, 
+  Component, 
+  ContentChild, 
+  DoCheck, 
+  ElementRef, 
+  Input, 
+  OnChanges, 
+  OnDestroy, 
+  OnInit, 
+  SimpleChanges,
+  ViewChild
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements 
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
+{
 
-  constructor() { }
+  @Input('srvElement') element : {type : string, name : string, content : string}
+  @Input() name : string;
+  @ViewChild('heading') header : ElementRef;
+  @ContentChild('contentParagraph') paragraph : ElementRef;
+
+  constructor() { 
+    console.log('constructor called!');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called!');
+    console.log(changes);
+  }
 
   ngOnInit(): void {
+    console.log('ngOnInit called!');
+    console.log('This content : ' + this.header.nativeElement.textContent);
+    console.log('This content of paragraph: ' + this.paragraph.nativeElement.textContent);
   }
+
+  ngDoCheck() {
+    console.log('ngDoCheck called!');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called!');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked called!');
+  }
+
+
+  ngAfterViewInit() {
+    console.log('ngAfteViewInit called!');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked called!');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy called');      
+  }
+  
 
 }
